d46fed7d79c78642b9c02b3cdca6cdcc22b66b45
Matthias Wilhelm <matthias.wilhelm@gus-group.com>
2024-04-04 10:35:47 +0200

    Added barcharts

--- pages/Detection_Birds_per_day.py
+++ pages/Detection_Birds_per_day.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import altair as alt
 
 st.set_page_config(
     page_title="Birdnet Analyzer",
     page_icon=':bird:'
 )
+
 
 def get_most_detections_per_day(confidence, ttl=3600):
     conn = st.connection('birds_db', type='sql')
@@ -18,6 +20,7 @@ def get_most_detections_per_day(confidence, ttl=3600):
                                        ttl=ttl, params={"confidence": confidence})
     return detections_per_day_df
 
+
 def get_least_detections_per_day(confidence, ttl=3600):
     conn = st.connection('birds_db', type='sql')
 
@@ -31,6 +34,7 @@ def get_least_detections_per_day(confidence, ttl=3600):
                                        ttl=ttl, params={"confidence": confidence})
     return detections_per_day_df
 
+
 if 'confidence' not in st.session_state:
     st.session_state['confidence'] = 70
 confidence = st.sidebar.slider("Confidence in %", max_value=99, min_value=70, value=st.session_state.confidence,
@@ -41,13 +45,16 @@ least_per_day_df = get_least_detections_per_day(confidence / 100)
 
 st.subheader("Most detected Birds per Day")
 
-col1, col2 = st.columns([2,5])
+col1, col2 = st.columns([2, 5])
 with col1:
     st.metric(f'Most Birds on {most_per_day_df["Date"][0]}', most_per_day_df['count'][0])
 with col2:
     st.write(most_per_day_df["name_list"][0])
 
-
+st.altair_chart(alt.Chart(most_per_day_df).mark_bar().encode(
+    x=alt.X('Date', sort=None),
+    y='count',
+), use_container_width=True)
 st.dataframe(most_per_day_df, column_config={
     "count": st.column_config.NumberColumn("Different birds", help="Unique detection of birds", width="small"),
     "date": st.column_config.DateColumn("First Detection", help="Date of the first detection", format="YYYY-MM-DD"),
@@ -56,11 +63,15 @@ st.dataframe(most_per_day_df, column_config={
 
 st.subheader("Least detected Birds per Day")
 
-col1, col2 = st.columns([2,5])
+col1, col2 = st.columns([2, 5])
 with col1:
     st.metric(f'Least Birds on {least_per_day_df["Date"][0]}', least_per_day_df['count'][0])
 with col2:
     st.write(least_per_day_df["name_list"][0])
+st.altair_chart(alt.Chart(least_per_day_df).mark_bar().encode(
+    x=alt.X('Date', sort=None),
+    y='count',
+), use_container_width=True)
 st.dataframe(least_per_day_df, column_config={
     "count": st.column_config.NumberColumn("Different birds", help="Unique detection of birds", width="small"),
     "date": st.column_config.DateColumn("First Detection", help="Date of the first detection", format="YYYY-MM-DD"),
--- pages/Detections_on_date.py
+++ pages/Detections_on_date.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 import streamlit as st
+import altair as alt
 
 st.set_page_config(
     page_title="Birdnet Analyzer",
     page_icon=':bird:'
 )
+
 
 def get_newest_bird_detections(confidence, date_detections, ttl=60):
     conn = st.connection('birds_db', type='sql')
@@ -20,6 +22,7 @@ def get_newest_bird_detections(confidence, date_detections, ttl=60):
 
     return birds_df
 
+
 if 'confidence' not in st.session_state:
     st.session_state['confidence'] = 70
 confidence = st.sidebar.slider("Confidence in %", max_value=99, min_value=70, value=st.session_state.confidence,
@@ -31,9 +34,17 @@ st.title(f'Birds on {date_detections}')
 
 birds_df = get_newest_bird_detections(confidence / 100, date_detections)
 
+number_detections = birds_df.groupby('Com_Name', as_index=False, sort=True)['Confidence'].count().rename(
+    columns={"Com_Name": "Common Name", "Confidence": "Count"}).sort_values(by='Count', ascending=False)
+# st.write(number_detections)
+st.altair_chart(alt.Chart(number_detections).mark_bar().encode(
+    x=alt.X('Common Name', sort=None),
+    y='Count',
+), use_container_width=True)
+
 st.dataframe(birds_df, column_config={
     "Date": st.column_config.DateColumn("Date", help="Date of the first detection",
-                                             format="YYYY-MM-DD"),
+                                        format="YYYY-MM-DD"),
     "Time": st.column_config.TimeColumn("Time", help="Time of the first detection"),
     "Confidence": st.column_config.NumberColumn("Confidence", format="%d", width="small"),
     "Sci_Name": "Scientific Name",
@@ -48,6 +59,3 @@ if birds_df.size > 0:
     if path.is_file():
         st.image(str(path) + '.png', caption=f'{bird_name}')
         st.audio(str(path))
-
-
-
